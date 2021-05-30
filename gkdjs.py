53b03f9abb6baf0cf57bca0a3d16366f950bde3e
wenjia03 <1070127864@qq.com>
2021-05-30 12:57:59 +0800

    Add files via upload

--- gkdjs.py
+++ gkdjs.py
@@ -11,7 +11,6 @@ import os.path
 import sys
 
 la = "https://area.sinaapp.com/bingImg/"
-lb =
 
 def save_img(img_url,dirname):
     #保存图片到磁盘文件夹dirname中
@@ -42,7 +41,7 @@ def get_img_url(raw_img_url = "https://area.sinaapp.com/bingImg/"):
     return img_url
 def set_img_as_wallpaper(filepath):
     ctypes.windll.user32.SystemParametersInfoW(20, 0, filepath, 0)
-
+print(len(sys.argv))
 print("Designed By Wenjia Chen Copyright 2012 -2020")
 print("Visit NEZHA.SPACE")
 print("Opensource on Github @wenjia03")
@@ -56,8 +55,10 @@ set_img_as_wallpaper(filepath)
 apploc ='D:'
 now = time.time()
 endunix = 1622995200
-re =  int((endunix -int(now) )/86400) + 1
-
+re =  int((endunix -int(now) )/86400)
+print('距离高考还有 '+str(re)+" 天")
+homework1 = ""
+homework2 = ""
 headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
 }
@@ -66,8 +67,12 @@ print("正在准备请求Hitokoto API……")
 bk_img = cv2.imread(str(apploc) + "\\in.jpg")
 #设置需要显示的字体
 fontpath = "D:\\ziti.ttf"
+fontpath2= "D:\\PHT.otf"
 font = ImageFont.truetype(fontpath, 48)
 font3 = ImageFont.truetype(fontpath, 40)
+font4 = ImageFont.truetype(fontpath2, 50)
+font5 = ImageFont.truetype(fontpath2, 50)
+
 font2 = ImageFont.truetype(fontpath,112)
 img_pil = Image.fromarray(bk_img)
 draw = ImageDraw.Draw(img_pil)
@@ -77,17 +82,22 @@ print("正在生成图片……")
 rep = rre.json()
 text = rep['hitokoto']
 info ="——《"+str(rep['from'])+"》  " +str(rep['from_who'])
-if re <= 5:
-    text = '春风得意马蹄疾，一日看尽长安花。'
-    info = '——《登科后》 孟郊'
 #绘制文字信息
 draw.text((850 , 350),  "距离高考还有", font = font, fill = (255, 255, 255))
 draw.text((850 , 400),  str(re) + "天", font = font2, fill = '#0000ff')
 draw.text((650 , 530),  text, font = font, fill = (255, 255, 255))
 draw.text((875 , 590),  info, font = font3, fill = (255, 255, 255))
+draw.text((500 , 640),  str(sys.argv[1]), font = font4, fill = str(sys.argv[7]))
+draw.text((500 , 690),  str(sys.argv[2]), font = font4, fill = str(sys.argv[7]))
+draw.text((500 , 740),  str(sys.argv[3]), font = font4, fill = str(sys.argv[7]))
+draw.text((500 , 790),  str(sys.argv[4]), font = font4, fill = str(sys.argv[7]))
+draw.text((500 , 840),  str(sys.argv[5]), font = font4, fill = str(sys.argv[7]))
+draw.text((500 , 890),  str(sys.argv[6]), font = font4, fill = str(sys.argv[7]))
+
 bk_img = np.array(img_pil)
 #cv2.imshow('test',bk_img)
 #cv2.waitKey()
+
 cv2.imwrite(str(apploc)+"\\out.jpg",bk_img)
 set_img_as_wallpaper(str(apploc)+"\\out.jpg")
 #designed by WenjiaChen on NEZHA.SPACE
